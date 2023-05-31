958dd7e2df68c2d095958a342af60922a8ec9368
Proclaim67 <135132899+Proclaim67@users.noreply.github.com>
2023-05-31 18:28:04 +0300

    Delete Probot Vote directory

--- Probot Vote/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-color 0A
-echo make sure you have Python installed.
-echo --------------------------------------
-echo if you do not have Python, open the install_python.bat file
-timeout /t 10
-color 09
-pip install -r requirements.txt
-pause
